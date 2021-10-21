1d038984675700c20a0ef68d01dc6a1344059cb0
wissen-satya <60770284+wissen-satya@users.noreply.github.com>
2021-10-21 20:43:20 +0530

    Update VmVnet.tf

--- VmVnet.tf
+++ VmVnet.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.71.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-
-  # More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-
-  subscription_id = "65911ece-b772-4e16-ab25-fc95982846dc"
-   client_id       = "37f075a8-9437-4317-84e7-8106b3c2c6a3"
-   client_secret   = "ui8lG0EanXUt.M3ZM-T8P8krICoaEk.RS6"
-   tenant_id       = "8a38d5c9-ff2f-479e-8637-d73f6241a4f0"
-}
 
 resource "azurerm_resource_group" "rg" {
   name = "RGSPJ"
